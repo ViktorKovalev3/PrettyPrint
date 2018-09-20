a7f1801ea2265e083d06ed720a59156f244468f5
Viktor Kovalev <13942538+ViktorKovalev3@users.noreply.github.com>
2018-09-20 21:24:09 +0300

    Init repo

---
+++ PrettyPrint.m
@@ -0,0 +1,27 @@
+(* ::Package:: *)
+
+BeginPackage["PrettyPrint`"];
+RemoveTrailingPoint;
+RemoveTrailingPoint::usage="Suppressing a trailing \[OpenCurlyDoubleQuote].\[CloseCurlyDoubleQuote] in output from Mathematica. Example: '5.' -> '5'
+In: S=1.22522;PrettyPrint`Variable[\"\!\(\*SubscriptBox[\(S\), \(var\)]\)\", S, 3];
+Out: \!\(\*SubscriptBox[\(S\), \(var\)]\)=1.23";
+PrintV;
+PrintV::usage="Print variable with precision and name";
+
+Begin["Private`"];
+PrintV[name_,var_,precision_:2] := Print[
+name,"=", PrettyPrint`RemoveTrailingPoint[NumberForm[N@var,precision]//ToString//ToExpression]
+];
+PrintV[name_,var_?MatrixQ,precision_:2] := Print[
+name,"=", PrettyPrint`RemoveTrailingPoint[NumberForm[N@var,precision]//ToString//ToExpression]//MatrixForm
+];
+
+RemoveTrailingPoint[x_]:=StringReplace[ToString@x,RegularExpression@"\\.$"->""]//ToExpression
+RemoveTrailingPoint[x_?ListQ]:=Map[RemoveTrailingPoint[#]&, x, 1];
+RemoveTrailingPoint[x_?MatrixQ]:=Map[RemoveTrailingPoint[#]&,x,2];
+End[];
+
+EndPackage[];
+
+
+
