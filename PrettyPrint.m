dd74dfd1f86087e5d740c7f745e33a9a234afeeb
Viktor Kovalev <13942538+ViktorKovalev3@users.noreply.github.com>
2018-09-28 10:45:50 +0300

    Refactoring + add single matrix variable realisation PrintV

--- PrettyPrint.m
+++ PrettyPrint.m
@@ -9,22 +9,25 @@ PrintV;
 PrintV::usage="Print variable with precision and name";
 
 Begin["Private`"];
-PrintV[var_,precision_?IntegerQ:2] := Print[
-PrettyPrint`RemoveTrailingPoint[NumberForm[N@var,precision]//ToString//ToExpression]
+(*4 all*)
+PrintV[var_,precision_Integer:2] := Print[
+RemoveTrailingPoint[NumberForm[N@var,precision]//ToString]
 ];
-PrintV[name_?StringQ,var_,precision_?IntegerQ:2] := Print[
-name,"=", PrettyPrint`RemoveTrailingPoint[NumberForm[N@var,precision]//ToString//ToExpression]
+PrintV[name_?StringQ,var_,precision_:2] := Print[
+name,"=", RemoveTrailingPoint[NumberForm[N@var,precision]//ToString]
 ];
-PrintV[name_?StringQ,var_?MatrixQ,precision_?IntegerQ:2] := Print[
+(*4 Matrix*)
+PrintV[var_?MatrixQ,precision_Integer:2] := Print[
+PrettyPrint`RemoveTrailingPoint[NumberForm[N@var,precision]//ToString//ToExpression]//MatrixForm
+];
+PrintV[name_?StringQ,var_?MatrixQ,precision_Integer:2] := Print[
 name,"=", PrettyPrint`RemoveTrailingPoint[NumberForm[N@var,precision]//ToString//ToExpression]//MatrixForm
 ];
 
-RemoveTrailingPoint[x_]:=StringReplace[ToString@x,RegularExpression@"\\.$"->""]//ToExpression
+
+RemoveTrailingPoint[x_]:=StringReplace[ToString@x,RegularExpression@"\\.$"->""]//ToExpression;
 RemoveTrailingPoint[x_?ListQ]:=Map[RemoveTrailingPoint[#]&, x, 1];
 RemoveTrailingPoint[x_?MatrixQ]:=Map[RemoveTrailingPoint[#]&,x,2];
 End[];
 
 EndPackage[];
-
-
-
