d9b861d9af681e50b83bb20ff68a4c11d8937b7f
Viktor Kovalev <13942538+ViktorKovalev3@users.noreply.github.com>
2018-09-28 11:04:21 +0300

    New style of lines; Fix 2 Quantor in input variable.

--- PrettyPrint.m
+++ PrettyPrint.m
@@ -7,15 +7,18 @@ In: S=1.22522;PrettyPrint`Variable[\"\!\(\*SubscriptBox[\(S\), \(var\)]\)\", S, 
 Out: \!\(\*SubscriptBox[\(S\), \(var\)]\)=1.23";
 PrintV;
 PrintV::usage="Print variable with precision and name";
-
 Begin["Private`"];
+
+
 (*4 all*)
-PrintV[var_,precision_Integer:2] := Print[
+PrintV[var_?(!StringQ),precision_Integer:2] := Print[
 RemoveTrailingPoint[NumberForm[N@var,precision]//ToString]
 ];
-PrintV[name_?StringQ,var_,precision_:2] := Print[
+PrintV[name_?StringQ,var_,precision_Integer:2] := Print[
 name,"=", RemoveTrailingPoint[NumberForm[N@var,precision]//ToString]
 ];
+
+
 (*4 Matrix*)
 PrintV[var_?MatrixQ,precision_Integer:2] := Print[
 PrettyPrint`RemoveTrailingPoint[NumberForm[N@var,precision]//ToString//ToExpression]//MatrixForm
@@ -28,6 +31,7 @@ name,"=", PrettyPrint`RemoveTrailingPoint[NumberForm[N@var,precision]//ToString/
 RemoveTrailingPoint[x_]:=StringReplace[ToString@x,RegularExpression@"\\.$"->""]//ToExpression;
 RemoveTrailingPoint[x_?ListQ]:=Map[RemoveTrailingPoint[#]&, x, 1];
 RemoveTrailingPoint[x_?MatrixQ]:=Map[RemoveTrailingPoint[#]&,x,2];
-End[];
 
+
+End[];
 EndPackage[];
